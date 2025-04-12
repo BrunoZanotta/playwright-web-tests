3458f9b09604b370355fbd63b4a7d51a36090587
BrunoZanotta <50785431+BrunoZanotta@users.noreply.github.com>
2025-04-11 22:01:54 -0300

    add first scenario

--- pages/login.page.js
+++ pages/login.page.js
@@ -1,14 +1,23 @@
-class LoginPage {
-    constructor(page) {
-      this.page = page;
-      this.usernameInput = page.locator('input[name="username"]');
-      this.passwordInput = page.locator('input[name="password"]');
-      this.loginButton = page.locator('button[type="submit"]');
-    }
-  
-    async login(username, password) {
-      await this.usernameInput.fill(username);
-      await this.passwordInput.fill(password);
-      await this.loginButton.click();
-    }
+require('dotenv').config()
+
+export class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.emailInput = page.locator('#user-name')
+    this.passwordInput = page.locator('#password')
+    this.submitButton = page.locator('#login-button')
   }
+
+  async gotoLoginPage() {
+    await this.page.goto('https://www.saucedemo.com');
+  }
+
+  async login() {
+    const username = process.env.USERNAME;
+    const password = process.env.PASSWORD;
+
+    await this.emailInput.fill(username)
+    await this.emailInput.fill(password)
+    await this.submitButton.click()
+  }
+}
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,9 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pages/login.page');
 
-test('should login successfully', async ({ page }) => {
+test('', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
-  await page.goto('https://example.com/login');
-  await loginPage.login('myUsername', 'myPassword');
-
-  // Verifique se você foi redirecionado para a página de dashboard
-  await expect(page).toHaveURL('https://example.com/dashboard');
+  await loginPage.gotoLoginPage();
+  await loginPage.login();
 });
